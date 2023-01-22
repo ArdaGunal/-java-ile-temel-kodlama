efda0f475b31be5a068b70c651301c9f4e6c82b4
ArdaGunal <ardagunalfb2001@gmail.com>
2023-01-22 20:57:49 +0300

    dosya eklendi

---
+++ donguler/src/elmas.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class elmas {
+    public static void main(String[] args) {
+
+        Scanner veri = new Scanner(System.in);
+        int a;
+        String bosluk ="-";
+        String sekil = "*";
+
+
+        System.out.print("Bir sayı giriniz :");
+        a=veri.nextInt();
+
+        for(int i=1;i<=a;i++){
+
+            for(int k=a-1;i<=k;k--){
+                System.out.print(bosluk);
+            }
+            for(int n=1;n<=2*i-1;n++){
+                System.out.print(sekil);
+            }
+            for(int k=a-1;i<=k;k--){
+                System.out.print(bosluk);
+            }
+            System.out.println();
+        }
+
+        //
+        for(int i=a;i>1;i--){
+
+            for(int k=a-i+1;k>=1;k--){
+                System.out.print(bosluk);
+            }
+            for(int n=1;n<=2*i-3;n++){
+                System.out.print(sekil);
+            }
+            for(int k=a-i+1;k>=1;k--){
+                System.out.print(bosluk);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
